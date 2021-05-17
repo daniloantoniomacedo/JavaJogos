6d16d9442424c9e9d6597f0e7f58571afb4e2062
daniloantoniomacedo <82525063+daniloantoniomacedo@users.noreply.github.com>
2021-05-17 19:35:19 -0300

    Add files via upload

---
+++ javaJogos/src/model/ModelJogoVelha.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.Random;
+
+public class ModelJogoVelha {
+	
+	public static String gerarPalavra() {
+		
+		String palavra = "";
+		int index;
+		
+		String[] dados = new String[5];
+		dados[0] = "Abacaxi";
+		dados[1] = "Goiaba";
+		dados[2] = "Berinjela";
+		dados[3] = "Tomate";
+		dados[4] = "Laranja";
+		Random aleatorio = new Random();
+		index = aleatorio.nextInt(4+1);
+		palavra = dados[index];
+		
+		return palavra;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(gerarPalavra());
+	}
+
+}
