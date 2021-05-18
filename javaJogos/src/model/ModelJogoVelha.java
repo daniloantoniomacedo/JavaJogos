585462cab056dcb8f42aaef399a190a11b345afd
daniloantoniomacedo <daniloantoniomacedo@gmail.com>
2021-05-17 22:46:01 -0300

    Código escrito até o dia 17/05.

--- javaJogos/src/model/ModelJogoVelha.java
+++ javaJogos/src/model/ModelJogoVelha.java
@@ -3,27 +3,30 @@ package model;
 import java.util.Random;
 
 public class ModelJogoVelha {
-	
-	public static String gerarPalavra() {
-		
+
+	public static char[] gerarPalavra() {
+
 		String palavra = "";
 		int index;
-		
+
 		String[] dados = new String[5];
 		dados[0] = "Abacaxi";
 		dados[1] = "Goiaba";
 		dados[2] = "Berinjela";
 		dados[3] = "Tomate";
-		dados[4] = "Laranja";
+		dados[4] = "Uva";
 		Random aleatorio = new Random();
 		index = aleatorio.nextInt(4+1);
-		palavra = dados[index];
+		palavra = dados[index];	
 		
-		//git teste 2
+		char[] letras = new char[palavra.length()];
+
+		for (int i=0; i<palavra.length(); i++) {
+			letras[i] = palavra.charAt(i);
+		}
 		
-		
-		return palavra;
-	}
+		return letras;
 	
+	}
 
 }
